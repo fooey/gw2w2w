bcf65e1ca809bcea3ab02e6f3e4c14cc998b665e
Jason Rushton <jason@jasonrushton.com>
2022-08-17 18:51:01 -0600

    remove jotai

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Provider as JotaiProvider } from 'jotai';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Index } from './containers/index/Index';
 import { Match } from './containers/match/Match';
@@ -16,16 +15,14 @@ export const queryClient = new QueryClient({
 
 export const App = () => {
   return (
-    <JotaiProvider>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Routes>
-            <Route index element={<Index />} />
-            <Route path=":matchId" element={<Match />} />
-          </Routes>
-        </BrowserRouter>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </JotaiProvider>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Index />} />
+          <Route path=":matchId" element={<Match />} />
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
