041075f236e75457a4501f806f0f795371d5801e
Jason Rushton <jason@jasonrushton.com>
2022-08-18 21:52:48 -0600

    move useLang into relevant queries

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -1,7 +1,8 @@
 import { QueryFunction, QueryKey, useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { Duration } from 'luxon';
-import { ApiLang, ApiWorld } from '../types/api';
+import { ApiWorld } from '../types/api';
+import { useLang } from './langs';
 
 // const { API_KEY } = import.meta.env;
 
@@ -25,20 +26,18 @@ export const defaultQueryFn: QueryFunction<unknown, QueryKey> = async ({ queryKe
 // const fetchWorld = (worldId: number): Promise<IApiWorld> =>
 //   axios.get(`https://api.guildwars2.com/v2/worlds?id=${worldId}`).then((response) => response.data);
 
-export const useWorlds = (lang: ApiLang = 'en') =>
-  useQuery<ApiWorld[]>([`/v2/worlds?lang=${lang}&ids=all`], {
+export const useWorlds = () => {
+  const lang = useLang();
+  return useQuery<ApiWorld[]>([`/v2/worlds?lang=${lang}&ids=all`], {
     cacheTime: Duration.fromObject({ days: 1 }).as('milliseconds'),
     staleTime: Duration.fromObject({ hour: 1 }).as('milliseconds'),
   });
+};
 
-export const useWorld = (lang: ApiLang = 'en', worldId: number) =>
-  useQuery<ApiWorld>([`/v2/worlds?lang=${lang}&ids=${worldId}`], {
+export const useWvwObjectives = () => {
+  const lang = useLang();
+  return useQuery<ApiWorld[]>([`/wvw/objectives?lang=${lang}&ids=all`], {
     cacheTime: Duration.fromObject({ days: 1 }).as('milliseconds'),
     staleTime: Duration.fromObject({ hour: 1 }).as('milliseconds'),
   });
-
-export const useWvwObjectives = (lang: ApiLang = 'en') =>
-  useQuery<ApiWorld[]>([`/wvw/objectives?lang=${lang}&ids=all`], {
-    cacheTime: Duration.fromObject({ days: 1 }).as('milliseconds'),
-    staleTime: Duration.fromObject({ hour: 1 }).as('milliseconds'),
-  });
+};
