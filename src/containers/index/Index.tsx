1cacf11f1f50ade37e65b5f5bfb49a66b8e44ac5
Jason Rushton <jason@jasonrushton.com>
2022-08-18 21:54:15 -0600

    reorganize

--- src/containers/index/Index.tsx
+++ src/containers/index/Index.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import classNames from 'classnames';
-import { Dictionary, keyBy, map } from 'lodash';
+import { Dictionary, keyBy, map, uniq } from 'lodash';
 import { Duration } from 'luxon';
 import React from 'react';
 import { Layout } from '../../components/layout/Layout';
@@ -10,11 +10,21 @@ import { useLang } from '../../utils/langs';
 
 const teams: WvwTeams[] = ['red', 'blue', 'green'];
 
-const regions = ['1', '2'];
-
 export const Index = () => {
-  const lang = useLang();
+  // const matchNumbers = uniq(map(matches, 'number')).sort();
 
+  return (
+    <Layout>
+      <main className="flex-auto">
+        <section className="bg-neutral mx-auto flex w-fit flex-col gap-4 rounded border bg-white py-4 px-8 shadow">
+          <MatchOverviews />
+        </section>
+      </main>
+    </Layout>
+  );
+};
+
+const MatchOverviews: React.FC = () => {
   const {
     isLoading: overviewIsLoading,
     error: overviewError,
@@ -40,84 +50,80 @@ export const Index = () => {
   const overviews = keyBy(overviewData, 'id');
   const scores = keyBy(scoresData, 'id');
 
-  const matchNumbersInit = new Set<string>();
-  const matchNumbersSet = matchIds.reduce((acc, matchId) => {
-    const [, number] = matchId.split('-');
-    return acc.add(number);
-  }, matchNumbersInit);
-
-  const matchNumbers: string[] = Array.from(matchNumbersSet);
-
-  // const matchNumbers = uniq(map(matches, 'number')).sort();
-
   return (
-    <Layout>
-      <main className="flex-auto">
-        <section className="bg-neutral mx-auto flex w-fit flex-col gap-4 rounded border bg-white py-4 px-8 shadow">
-          <table>
-            <tbody>
-              {matchIds.map((matchId, index) => {
-                const isNewRegion = index > 0 && matchId.split('-')[0] !== matchIds[index - 1].split('-')[0];
+    <table>
+      <tbody>
+        {matchIds.map((matchId, index) => {
+          const isNewRegion = index > 0 && matchId.split('-')[0] !== matchIds[index - 1].split('-')[0];
 
-                return (
-                  <React.Fragment key={matchId}>
-                    {isNewRegion ? (
-                      <tr>
-                        <th colSpan={3} className="py-4">
-                          <hr />
-                        </th>
-                      </tr>
-                    ) : null}
+          return (
+            <React.Fragment key={matchId}>
+              {isNewRegion ? (
+                <tr>
+                  <th colSpan={6} className="py-4">
+                    <hr />
+                  </th>
+                </tr>
+              ) : null}
 
-                    <tr key={matchId} className="even:bg-slate-50">
-                      <OverviewMatch key={matchId} matchId={matchId} overviews={overviews} scores={scores} />
-                    </tr>
-                  </React.Fragment>
-                );
-              })}
-            </tbody>
-          </table>
-        </section>
-      </main>
-    </Layout>
+              <tr key={matchId} className={`even:bg-slate-50`}>
+                <MatchOverview key={matchId} matchId={matchId} overviews={overviews} scores={scores} />
+              </tr>
+            </React.Fragment>
+          );
+        })}
+      </tbody>
+    </table>
   );
-};;
+};
 
-interface IOverviewMatchProps {
+interface IMatchOverviewProps {
   matchId: string;
   overviews: Dictionary<ApiMatchOverview>;
   scores: Dictionary<ApiMatchScores>;
 }
-const OverviewMatch: React.FC<IOverviewMatchProps> = ({ matchId, overviews, scores }) => {
+const MatchOverview: React.FC<IMatchOverviewProps> = ({ matchId, overviews, scores }) => {
   const lang = useLang();
 
   const matchOverview = overviews[matchId];
   const matchScores = scores[matchId];
   const worldsByTeams = matchOverview.all_worlds;
 
+  const orderedScores = [matchScores.scores.red, matchScores.scores.blue, matchScores.scores.green].sort().reverse();
+  const uniqueScores = uniq(orderedScores);
+
   return (
     <>
       {teams.map((teamColor) => {
         const teamWorlds = worldsByTeams[teamColor].filter((id) => id < 10000);
         const teamScore = matchScores.scores[teamColor];
+        const rank = uniqueScores.indexOf(teamScore) + 1;
 
         return (
-          <td key={`${matchId}:${teamColor}`} className="align-top">
-            <div
-              className={classNames(`flex flex-col gap-1 py-2 px-4`, {
-                'text-green-900': teamColor === 'green',
-                'text-red-900': teamColor === 'red',
-                'text-blue-900': teamColor === 'blue',
-              })}
-            >
-              <div>{teamScore.toLocaleString(lang, {})}</div>
-              <div className="text-sm">
-                {teamWorlds.map((worldId) => (
-                  <WorldName key={worldId} worldId={worldId} />
-                ))}
+          <React.Fragment key={`${matchId}:${teamColor}`}>
+            <td className="align-top">
+              <div
+                className={classNames(`flex flex-col gap-1 py-2 px-4`, {
+                  'text-green-900': teamColor === 'green',
+                  'text-red-900': teamColor === 'red',
+                  'text-blue-900': teamColor === 'blue',
+                })}
+              >
+                <div
+                  className={classNames(`text-xl`, {
+                    'font-bold': rank === 1,
+                  })}
+                >
+                  {teamScore.toLocaleString(lang, {})}
+                </div>
+                <div className="text-sm">
+                  {teamWorlds.map((worldId) => (
+                    <WorldName key={worldId} worldId={worldId} />
+                  ))}
+                </div>
               </div>
-            </div>
-          </td>
+            </td>
+          </React.Fragment>
         );
       })}
     </>
