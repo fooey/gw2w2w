80169caec3656a0d01eab49d714659ff59c8dc11
Jason Rushton <jason@jasonrushton.com>
2022-08-19 21:21:52 -0600

    better footer wrapping

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,26 +6,22 @@ import pkgJson from '../../../package.json';
 export const Footer = () => {
   return (
     <footer className="mt-20 flex-none ">
-      <div className="mx-auto my-10 flex max-w-3xl flex-col place-content-center gap-4 text-center text-xs text-slate-700">
+      <div className="mx-auto my-10 flex max-w-xl flex-col place-content-center gap-4 text-center text-xs text-slate-700">
         <div className="flex flex-row items-center justify-center">
-          <div className="flex w-64 flex-col items-center justify-center">
+          <p className="flex w-64 flex-col items-center justify-center">
             <MdEmail className="text-6xl" />
-            <p>
-              Please send comments and bugs to{' '}
-              <a className="whitespace-nowrap" href="mailto:website@gw2w2w.com">
-                website@gw2w2w.com
-              </a>
-            </p>
-          </div>
-          <div className="flex w-64 flex-col items-center justify-center">
+            <span>Please send comments and bugs to</span>
+            <a className="whitespace-nowrap" href="mailto:website@gw2w2w.com">
+              website@gw2w2w.com
+            </a>
+          </p>
+          <p className="flex w-64 flex-col items-center justify-center">
             <AiFillGithub className="text-6xl" />
-            <p>
-              Source available at{' '}
-              <a className="whitespace-nowrap" href="https://github.com/fooey/gw2w2w">
-                github.com/fooey/gw2w2w
-              </a>
-            </p>
-          </div>
+            <span>Source available at</span>
+            <a className="whitespace-nowrap" href="https://github.com/fooey/gw2w2w">
+              github.com/fooey/gw2w2w
+            </a>
+          </p>
         </div>
         <p>
           <MdCopyright className="inline p-0" /> 2013 ArenaNet, Inc. All rights reserved. NCsoft, the interlocking NC
