502054057eccfaca8926e7836dbfb40ae33888ec
Jason Rushton <jason@jasonrushton.com>
2022-08-19 21:22:00 -0600

    remove console

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -3,7 +3,6 @@ const { DateTime } = require('luxon');
 const path = require('path');
 
 const packagePath = path.join(__dirname, '..');
-console.log(`🚀 ~ file: build.js ~ line 6 ~ packagePath`, packagePath);
 
 PackageJson.load(packagePath).then((pkgJson) => {
   pkgJson.update({ buildDate: DateTime.local().toISO() });
