e19d2dc7ca84be04b0026e94313d12c623624e22
Jason Rushton <jason@jasonrushton.com>
2022-08-21 20:45:47 -0600

    add persistQueryClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { persistQueryClient } from '@tanstack/react-query-persist-client';
+import { Duration } from 'luxon';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { MatchOverviewsContainer } from '~/containers/index/Index';
 import { World } from '~/containers/world/World';
@@ -9,8 +12,15 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       queryFn: defaultQueryFn,
+      cacheTime: Duration.fromObject({ hours: 24 }).as('milliseconds'),
     },
   },
+});
+
+const localStoragePersister = createSyncStoragePersister({ storage: window.localStorage });
+persistQueryClient({
+  queryClient,
+  persister: localStoragePersister,
 });
 
 export const App = () => {
